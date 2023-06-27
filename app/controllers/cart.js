bebe9cc0af90aebc99eb4f79f8f71cf30f9c443f
Jorge <barbozajorge@gmail.com>
2023-06-27 10:43:11 -0400

    Automatically calculating Subtotal, Taxes and Total

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,3 +1,15 @@
 import Controller from '@ember/controller';
 
-export default class CartController extends Controller {}
+export default class CartController extends Controller {
+  get subtotal() {
+    return this.model.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+  }
+  get tax() {
+    return (0.07 * this.subtotal).toFixed(2);
+  }
+  get total() {
+    return (parseFloat(this.subtotal) + parseFloat(this.tax)).toFixed(2);
+  }
+}
